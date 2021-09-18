f4050c827a7744539c0ce72ec508523e65469748
Crowdbotics <team@crowdbotics.com>
2021-09-18 14:59:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "radio_paranoia_30659Identifier",
+  url: "https://radio_paranoia_30659Identifier.botics.co",
+  api: "https://radio_paranoia_30659Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
